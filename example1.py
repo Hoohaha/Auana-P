0fe63c5f651c1f4d65e5631fee211085b0b45e2d
Haley <B51762@freescale.com>
2015-01-07 15:19:54 +0800

    update example
    Signed-off-by: Haley <B51762@freescale.com>

--- example1.py
+++ example1.py
@@ -4,12 +4,12 @@ current_directory = os.path.dirname(os.path.abspath(__file__)).replace('\\','/')
 
 '''==================save_fingerprint======================'''
 start = time.time()
-Fana(current_directory+"/sample/ding.wav").save_fingerprint()
+Fana(current_directory+"/sample/ding.wav").pre()
 print time.time()-start
 print " "
 
 start = time.time()
-Fana(current_directory+"/sample/source1.wav").save_fingerprint()
+Fana(current_directory+"/sample/source1.wav").pre()
 print time.time()-start
 print " "
 
@@ -18,11 +18,15 @@ print " "
 # print time.clock()-start
 # print " "
 
-# start = time.clock()
-# Fana(current_directory+"/sample/Come And Get Your Love.wav").save_fingerprint()
-# print time.clock()-start
-# print " "
+start = time.clock()
+Fana("E:/Come And Get Your Love.wav").pre()
+print time.clock()-start
+print " "
 
+start = time.clock()
+print Fana("E:/Come And Get Your Love.wav").stereo_start()
+print time.clock()-start
+print " "
 # '''===================function example====================='''
 # print Fana(current_directory+"/sample/piano.wav").stereo_start()
 # print" "
@@ -38,7 +42,7 @@ file_num = 0
 for parent, dirnames, filenames in os.walk(dir_audio):
 	for filename in filenames:
 		portion = os.path.splitext(filename)
-		if portion[1] == '.wav'and (211 <= int(portion[0]) <= 215 or 111 <= int(portion[0]) <= 116 or 121 <= int(portion[0]) <= 126) :
+		if portion[1] == '.wav' :
 			path = os.path.join(parent, filename)
 			path = path.replace('\\','/')
 			audio_list.append(path)
@@ -48,6 +52,5 @@ failed=[]
 #start to handle
 for a in  xrange(len(audio_list)):#
 	start = time.clock()
-	print audio_list[a]
 	res = Fana(audio_list[a]).stereo_start()
 	print audio_list[a],res,time.clock()-start
